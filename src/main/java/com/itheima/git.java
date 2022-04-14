cb80698a876290764dd33f0e1b6e72b767489e0f
ItYaozexin <103585397+ItYaozexin@users.noreply.github.com>
2022-04-14 15:55:16 +0800

    Update git.java

--- src/main/java/com/itheima/git.java
+++ src/main/java/com/itheima/git.java
@@ -8,5 +8,7 @@ public class git {
         System.out.println ("hello,world4" );
         System.out.println ("master test" );
         System.out.println ("hot-fix test" );
+        System.out.println ("push test");
+        System.out.println ("pull test")
     }
 }
